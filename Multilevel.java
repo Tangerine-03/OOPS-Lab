eb02b2f7981756602f388645f411b24685177ead
Joel Joy <joeljoypathalil@gmail.com>
2026-03-13 14:52:42 +0530

    Add files via upload

---
+++ Multilevel.java
@@ -0,0 +1,28 @@
+class Principal
+{
+    String name="Rakesh";
+}
+class Teacher extends Principal
+{
+    int tchrId=56;
+}
+class Student extends Teacher
+{
+    int rno=78;
+    void display()
+    {
+        System.out.println("Principal name:"+name);
+        System.out.println("Teacher id:"+tchrId);
+        System.out.println("Student roll no:"+rno);
+    }
+}
+class Multilevel 
+{
+    public static void main(String args[])
+    {
+       Student ob=new Student();
+        ob.display();
+    }
+}
+
+
